83f36a24861be766d6483aa2acbea97d772b4ebf
Prajjwal Chauhan <83890311+Prajjwal-Chauhan@users.noreply.github.com>
2023-06-29 15:37:00 +0530

    sort in O(n) question added

---
+++ array/sort_0_1.java
@@ -0,0 +1,19 @@
+public class sort_0_1 {
+    public static void main(String[] args) {
+        int[] arr = {0,1,1,0,1,1,1,0,1,0,0};
+        int count = 0;
+        for (int i : arr) {
+            if(i == 0) count++;
+        }
+        
+        for (int i = 0; i < count; i++) {
+            arr[i] = 0;
+        }
+        for (int i = count; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
