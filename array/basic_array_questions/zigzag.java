b75a0191dd6f25b4322b671e715c61fd4ed9afdc
Prajjwal Chauhan <83890311+Prajjwal-Chauhan@users.noreply.github.com>
2023-06-30 13:21:32 +0530

    Zig Zag question added

---
+++ array/basic_array_questions/zigzag.java
@@ -0,0 +1,34 @@
+/**
+ * zigzag
+ */
+public class zigzag {
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,7,8,6,2,1};
+        boolean flag = true;
+        int temp = 0;
+
+        for (int i = 0; i < arr.length-1; i++) {
+            if(flag){
+                if(arr[i] > arr[i+1]){
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+
+                }
+            }
+            else {
+                if(arr[i] < arr[i+1]){
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+    
+                }
+            }
+        }
+
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+    }
+}
