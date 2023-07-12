b6f0d56359868996040c67e9b90b5a2081982144
Prajjwal Chauhan <83890311+Prajjwal-Chauhan@users.noreply.github.com>
2023-07-12 17:09:07 +0530

    Basic Binary tree Depth first traversal committed.

---
+++ tree/BinaryTree.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+class BinaryTreeNode<T>{
+    T data;
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
+
+    BinaryTreeNode(T data){
+        this.data = data;
+    }
+}
+
+class BinaryTreeOperations{
+    String nodeName = "root";
+    Scanner sc = new Scanner(System.in);
+
+    BinaryTreeNode<Integer> insert(){
+        System.out.println("Enter the " +nodeName+ " Data or enter -1 to exit");
+        int data = sc.nextInt();
+        if(data == -1) return null;
+        BinaryTreeNode<Integer> node = new BinaryTreeNode<>(data);
+        nodeName = "left";
+        node.left = insert();
+        nodeName = "right";
+        node.right = insert();
+        nodeName = "root";
+         return node;
+    }
+
+    void print(BinaryTreeNode<Integer> root){
+        if(root == null) return;
+        String result = "";
+        result += root.data + "->";
+        if(root.left != null) result += " Left "+root.left.data;
+        if(root.right != null) result += " Right "+root.right.data;
+        System.out.println(result);
+        print(root.left);
+        print(root.right);
+    }
+
+    void preOrder(BinaryTreeNode<Integer> root){
+        if(root == null){
+            return;
+        }
+
+        System.out.println(root.data);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    
+    void inOrder(BinaryTreeNode<Integer> root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.data);
+        inOrder(root.right);
+    }
+
+    void postOrder(BinaryTreeNode<Integer> root){
+        if(root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.data);
+    }
+}
+
+/**
+ * BinaryTree
+ */
+public class BinaryTree {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinaryTreeOperations opr = new BinaryTreeOperations();
+        BinaryTreeNode<Integer> root = null;
+        while(true){
+            System.out.println("Binary Tree Operations");
+            System.out.println("1> Insert");
+            System.out.println("2> Print");
+            System.out.print("Enter Choice :");
+            int n = sc.nextInt();
+            switch (n) {
+                case 1:
+                    root = opr.insert();
+                    break;
+
+                case 2:
+                    opr.print(root);
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+        // BinaryTreeNode<String> b = new BinaryTreeNode<>();
+    }
+}
